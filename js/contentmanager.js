5e0a11e2dd16eb57792fff79bd614b19fc5d9d12
Suresh Peddapu <suresh.peddapu@accenture.com>
2014-01-03 19:08:53 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -214,6 +214,40 @@ function fromTagSpaceRequest(selectedCombo,val,placeType) {
 }
 //end tag
 
+//start category
+function getCategoryPlace(obj) {
+var placeType="";
+ console.log($("#"+obj.id).find('option:selected').val());
+ var val = $("#"+obj.id).find('option:selected').val();
+ if(val == "Space")
+placeType = "space";
+else if(val == "Group")
+placeType ="group";
+else
+placeType = "project";
+var placeName = fromCategorySpaceRequest(obj.id,val,placeType);
+$("#"+obj.id).find('option:first').attr('selected', 'selected');
+console.log("place name : "+placeName);
+}
+
+function fromCategorySpaceRequest(selectedCombo,val,placeType) {
+  var place_name = "";
+  var params = {
+        type: ""+placeType,
+        success: (function (data) {
+            //consolelog("DATA: "+JSON.stringify(data));			
+            place_name = data.name;
+			 if(place_name!='' && selectedCombo == "fromCategoryPlace")
+               $('#fromCategoryLabel').text(val+" : "+place_name);
+        }),
+        error: handleResponse
+    };
+    // calling the OSAPI with the params. final call.
+    osapi.jive.corev3.places.requestPicker(params);
+	return place_name;
+}
+//end category
+
 function handleResponse(data) {
     alert("Error in Application..!!");
     console.log(data);
