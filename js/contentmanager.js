5f0869a84bbe2d6eed0e19da4b28641a5455da85
Suresh Peddapu <suresh.peddapu@accenture.com>
2014-01-03 18:47:20 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -19,11 +19,8 @@ function fromSpaceRequest(selectedCombo,val,placeType) {
   var params = {
         type: ""+placeType,
         success: (function (data) {
-            //consolelog("DATA: "+JSON.stringify(data));
-			
+            //consolelog("DATA: "+JSON.stringify(data));			
             place_name = data.name;
-			console.log(place_name);
-			
 			/*
 			var  fromLabelStr = $('#fromLabel').text();
 			fromLabelStr = fromLabelStr.substring(fromLabelStr.indexOf(":")+1,fromLabelStr.length);
@@ -31,8 +28,7 @@ function fromSpaceRequest(selectedCombo,val,placeType) {
 			var  toLabelStr = $('#toLabel').text();
 			toLabelStr = fromLabelStr.substring(toLabelStr.indexOf(":")+1,toLabelStr.length);
 			console.log(toLabelStr);
-			*/
-			
+			*/			
 			if(place_name!='' && selectedCombo == "fromPlace")
                $('#fromLabel').text(val+" : "+place_name);		
 			  else if(place_name!=''&& selectedCombo == "toPlace")
@@ -45,6 +41,7 @@ function fromSpaceRequest(selectedCombo,val,placeType) {
 	return place_name;
 }
 //end copy
+
 //start move
 function getMovePlace(obj) {
 var placeType="";
@@ -66,20 +63,8 @@ function fromMoveSpaceRequest(selectedCombo,val,placeType) {
   var params = {
         type: ""+placeType,
         success: (function (data) {
-            //consolelog("DATA: "+JSON.stringify(data));
-			
+            //consolelog("DATA: "+JSON.stringify(data));			
             place_name = data.name;
-			console.log(place_name);
-			
-			/*
-			var  fromLabelStr = $('#fromLabel').text();
-			fromLabelStr = fromLabelStr.substring(fromLabelStr.indexOf(":")+1,fromLabelStr.length);
-			console.log(fromLabelStr);
-			var  toLabelStr = $('#toLabel').text();
-			toLabelStr = fromLabelStr.substring(toLabelStr.indexOf(":")+1,toLabelStr.length);
-			console.log(toLabelStr);
-			*/
-			
 			 if(place_name!='' && selectedCombo == "fromMovePlace")
                $('#fromMoveLabel').text(val+" : "+place_name);		
 			  else if(place_name!=''&& selectedCombo == "toMovePlace")
@@ -114,20 +99,8 @@ function fromDeleteSpaceRequest(selectedCombo,val,placeType) {
   var params = {
         type: ""+placeType,
         success: (function (data) {
-            //consolelog("DATA: "+JSON.stringify(data));
-			
+            //consolelog("DATA: "+JSON.stringify(data));			
             place_name = data.name;
-			console.log(place_name);
-			
-			/*
-			var  fromLabelStr = $('#fromLabel').text();
-			fromLabelStr = fromLabelStr.substring(fromLabelStr.indexOf(":")+1,fromLabelStr.length);
-			console.log(fromLabelStr);
-			var  toLabelStr = $('#toLabel').text();
-			toLabelStr = fromLabelStr.substring(toLabelStr.indexOf(":")+1,toLabelStr.length);
-			console.log(toLabelStr);
-			*/
-			
 			 if(place_name!='' && selectedCombo == "fromDeletePlace")
                $('#fromDeleteLabel').text(val+" : "+place_name);
         }),
@@ -138,6 +111,40 @@ function fromDeleteSpaceRequest(selectedCombo,val,placeType) {
 	return place_name;
 }
 //end delete
+
+//start upload
+function getUploadPlace(obj) {
+var placeType="";
+ console.log($("#"+obj.id).find('option:selected').val());
+ var val = $("#"+obj.id).find('option:selected').val();
+ if(val == "Space")
+placeType = "space";
+else if(val == "Group")
+placeType ="group";
+else
+placeType = "project";
+var placeName = fromUploadSpaceRequest(obj.id,val,placeType);
+$("#"+obj.id).find('option:first').attr('selected', 'selected');
+console.log("place name : "+placeName);
+}
+
+function fromUploadSpaceRequest(selectedCombo,val,placeType) {
+  var place_name = "";
+  var params = {
+        type: ""+placeType,
+        success: (function (data) {
+            //consolelog("DATA: "+JSON.stringify(data));			
+            place_name = data.name;
+			 if(place_name!='' && selectedCombo == "fromUploadPlace")
+               $('#fromUploadLabel').text(val+" : "+place_name);
+        }),
+        error: handleResponse
+    };
+    // calling the OSAPI with the params. final call.
+    osapi.jive.corev3.places.requestPicker(params);
+	return place_name;
+}
+//end upload
 
 function handleResponse(data) {
     alert("Error in Application..!!");
